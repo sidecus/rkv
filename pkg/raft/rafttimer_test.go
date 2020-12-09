ecb783d98676b967fd73c77f671adad003d50d97
sidecus <4399408+sidecus@users.noreply.github.com>
2020-12-09 01:19:23 -0800

    improve rafttimer to avoid stale events (this mitigates potential election storm when a new node is added)

--- pkg/raft/rafttimer_test.go
+++ pkg/raft/rafttimer_test.go
@@ -7,14 +7,16 @@ import (
 // Fake timer implementation for other unit tests
 type fakeRaftTimer struct {
 	state NodeState
+	term  int
 }
 
 func (t *fakeRaftTimer) Start() {
 }
 func (t *fakeRaftTimer) Stop() {
 }
-func (t *fakeRaftTimer) Refresh(newState NodeState) {
+func (t *fakeRaftTimer) Refresh(newState NodeState, term int) {
 	t.state = newState
+	t.term = term
 }
 
 func TestRaftTimer(t *testing.T) {
